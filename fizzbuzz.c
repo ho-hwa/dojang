eed0243453253b874fa6530a3269e5abce52f96f
purificationheo <purification_heo@naver.com>
2017-07-09 16:54:54 +0900

    fizzbuzz.c using operator

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -5,17 +5,8 @@
 int main(){
 
 	for(int i = 0; i< 100; i ++){
-		if(i%15 == 0){
-			printf("FizzBuzz\n");
-		}
-		else if(i%3==0){
-			printf("Fizz\n");
-		}
-		else if(i%5==0){
-			printf("Buzz\n");
-		}
-		else 
-			printf("%d\n",i);
+		printf(i%3 ? i%5 ? "%d\n":"Buzz\n": i%5 ? "Fizz\n" :"FizzBuzz\n", i);
+
 	}
 
 	return 0;
